37c14e3126b0097526f71e8138de114bdb714fd8
xbinh <binhnguyenxuan47@gmail.com>
2025-07-13 17:51:08 +0700

    fix

--- program.py
+++ program.py
@@ -3,8 +3,22 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 from PyQt6.QtCore import *
 from PyQt6 import uic
+from PyQt6.QtMultimedia import *
+from PyQt6.QtMultimediaWidgets import *
 import sys
 from setup_db import *
+import os
+
+def normalize_path(path):
+    # Convert path separators to system-specific format
+    normalized = os.path.normpath(path)
+    # Check if path exists, if not try to find it relative to current directory
+    if not os.path.exists(normalized):
+        # Try relative to current directory
+        relative_path = os.path.join(os.getcwd(), normalized)
+        if os.path.exists(relative_path):
+            return relative_path
+    return normalized
 
 class MessageBox():
     def success_box(self, message) :
@@ -162,6 +176,59 @@ class Register(QMainWindow) :
                 return False
             return '.' in s[idx_at+1:]
         
+class MovieItemWidget(QWidget):
+    signal_detail_movie = pyqtSignal(int)
+    signal_play_movie = pyqtSignal(int)
+    signal_favorite_changed = pyqtSignal()  # New signal for favorite changes
+    
+    def __init__(self, movie_id, title, banner_path, video_path, description):
+        super().__init__()
+        self.movie_id = movie_id
+        self.user_id = 1  # We'll get this from Main class
+        
+        # Load UI
+        uic.loadUi('ui/movie_item.ui', self)
+        
+        # Set data
+        self.titleLabel.setText(title)
+        
+        # Set banner with normalized path
+        banner_path = normalize_path(banner_path)
+        pixmap = QPixmap(banner_path)
+        self.bannerLabel.setPixmap(pixmap)
+        
+        # Connect signals
+        self.infoButton.clicked.connect(self.show_detail)
+        self.playButton.clicked.connect(self.play_movie)
+        self.btn_favorite.clicked.connect(self.toggle_favorite)
+        
+        # Set favorite icon
+        self.update_favorite_icon()
+        
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+        self.update_favorite_icon()
+        
+    def update_favorite_icon(self):
+        is_fav = is_favorite(self.user_id, self.movie_id)
+        icon = QIcon("img/heart-solid.svg" if is_fav else "img/heart-regular.svg")
+        self.btn_favorite.setIcon(icon)
+        
+    def show_detail(self):
+        self.signal_detail_movie.emit(self.movie_id)
+        
+    def play_movie(self):
+        self.signal_play_movie.emit(self.movie_id)
+        
+    def toggle_favorite(self):
+        is_fav = is_favorite(self.user_id, self.movie_id)
+        if is_fav:
+            remove_from_favorites(self.user_id, self.movie_id)
+        else:
+            add_to_favorites(self.user_id, self.movie_id)
+        self.update_favorite_icon()
+        self.signal_favorite_changed.emit()  # Emit signal when favorite status changes
+        
 class Home(QMainWindow) :
     def __init__(self, user_id) :
         super().__init__()
@@ -259,6 +326,380 @@ class Home(QMainWindow) :
             self.user["avatar"] = file
             self.lb_avatar.setPixmap(QPixmap(file))
             update_user_avatar(self.user_id, file)
+    
+    def setupUI(self):
+        # Setup movie container
+        self.movieList = QScrollArea()
+        self.movieList.setStyleSheet("""
+            QScrollArea {
+                background-color: black;
+                border: none;
+            }
+            QScrollBar:vertical {
+                border: none;
+                background: rgb(45, 45, 45);
+                width: 10px;
+                margin: 0;
+            }
+            QScrollBar::handle:vertical {
+                background: rgb(80, 80, 80);
+                min-height: 20px;
+                border-radius: 5px;
+            }
+            QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+                border: none;
+                background: none;
+            }
+        """)
+        
+        self.movieItem = QWidget()
+        self.gridLayout = QGridLayout(self.movieItem)
+        self.gridLayout.setContentsMargins(10, 10, 10, 10)
+        self.gridLayout.setSpacing(10)
+
+        self.movieItem.setLayout(self.gridLayout)
+        self.movieList.setWidget(self.movieItem)
+        self.movieList.setWidgetResizable(True)
+        
+        # Add movie list to video container
+        containerLayout = QVBoxLayout()
+        containerLayout.addWidget(self.movieList)
+        self.video_container.setLayout(containerLayout)
+        
+        # Setup favorite container with scroll area
+        self.favoriteList = QScrollArea()
+        self.favoriteList.setStyleSheet("""
+            QScrollArea {
+                background-color: black;
+                border: none;
+            }
+            QScrollBar:vertical {
+                border: none;
+                background: rgb(45, 45, 45);
+                width: 10px;
+                margin: 0;
+            }
+            QScrollBar::handle:vertical {
+                background: rgb(80, 80, 80);
+                min-height: 20px;
+                border-radius: 5px;
+            }
+            QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical {
+                border: none;
+                background: none;
+            }
+        """)
+        
+        self.favoriteItem = QWidget()
+        self.favoriteLayout = QGridLayout(self.favoriteItem)
+        self.favoriteLayout.setContentsMargins(10, 10, 10, 10)
+        self.favoriteLayout.setSpacing(10)
+        
+        self.favoriteItem.setLayout(self.favoriteLayout)
+        self.favoriteList.setWidget(self.favoriteItem)
+        self.favoriteList.setWidgetResizable(True)
+        
+        # Add favorite list to favorite container
+        favoriteContainerLayout = QVBoxLayout()
+        favoriteContainerLayout.addWidget(self.favoriteList)
+        self.favorite_container.setLayout(favoriteContainerLayout)
+        
+        self.txt_search = self.findChild(QLineEdit, 'txt_search')
+        self.btn_search = self.findChild(QPushButton, 'btn_search')
+        self.btn_search.clicked.connect(self.search_movie)
+        
+        # Setup media player components
+        self.setupMediaPlayer()
+    
+    def setupMediaPlayer(self):
+        # Setup media player
+        self.lbl_title = self.findChild(QLabel, 'videoName')
+        self.volumeBtn = self.findChild(QPushButton, 'volumeBtn')
+        self.timeLabel = self.findChild(QLabel, 'timeLabel')
+        self.durationBar = self.findChild(QSlider, 'durationBar')
+        self.volumeBar = self.findChild(QSlider, 'volumeBar')
+        self.videoName = self.findChild(QLabel, 'videoName')
+        self.playBtn = self.findChild(QPushButton, 'playBtn')
+        
+        # Load icons
+        self.playIcon = QIcon("img/play-solid.svg")
+        self.pauseIcon = QIcon("img/pause-solid.svg")
+        self.volumeHighIcon = QIcon("img/volume-high-solid.svg")
+        self.volumeLowIcon = QIcon("img/volume-low-solid.svg")
+        self.volumeOffIcon = QIcon("img/volume-off-solid.svg")
+        
+        # Setup video player buttons
+        self.playBtn.setIcon(self.playIcon)
+        self.volumeBtn.setIcon(self.volumeHighIcon)
+        
+        # Setup volume control
+        self.current_volume = 50
+        self.volumeBar.setValue(self.current_volume)
+        self.volumeBar.setRange(0, 100)
+        self.volumeBar.setValue(50)
+        
+        # Connect signals
+        self.playBtn.clicked.connect(self.play)
+        self.volumeBtn.clicked.connect(self.toggleMute)
+        
+        # Setup video widget
+        placeholder = self.findChild(QWidget, 'videoWidget')
+        self.videoWidget = QVideoWidget()
+        self.videoWidget.setGeometry(placeholder.geometry())
+        self.videoWidget.setParent(placeholder.parentWidget())
+        placeholder.hide()
+        
+        # Setup media player
+        self.mediaPlayer = QMediaPlayer(self)
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        self.audioOutput = QAudioOutput(self)
+        self.mediaPlayer.setAudioOutput(self.audioOutput)
+
+    def loadMovies(self):
+        # Get all movies from database
+        movies = get_all_videos()
+        if movies:
+            self.renderMovieList(movies)
+        
+    def renderMovieList(self, movie_list: list):
+        # Clear previous search results
+        for i in reversed(range(self.gridLayout.count())):
+            widgetToRemove = self.gridLayout.itemAt(i).widget()
+            if widgetToRemove:
+                self.gridLayout.removeWidget(widgetToRemove)
+                widgetToRemove.setParent(None)
+            
+        row = 0
+        column = 0
+        for movie in movie_list:
+            itemWidget = MovieItemWidget(
+                movie["id"], 
+                movie["title"], 
+                movie["banner"],  
+                movie["video_path"],  
+                movie.get("description", "")
+            )
+            itemWidget.set_user_id(self.user_id)  # Set user_id for the widget
+            itemWidget.signal_detail_movie.connect(self.catch_detail_movie)
+            itemWidget.signal_play_movie.connect(self.catch_play_movie)
+            itemWidget.signal_favorite_changed.connect(self.on_favorite_changed)  # Connect new signal
+            self.gridLayout.addWidget(itemWidget, row, column)
+            column += 1
+            if column == 3:
+                row += 1
+                column = 0
+    
+    def search_movie(self):
+        name = self.txt_search.text()
+        movie_list = get_video_by_name(name)
+        self.renderMovieList(movie_list)
+
+    def detail_movie(self, movie_id):
+        movie = get_video_by_id(movie_id)
+        if not movie:
+            return
+            
+        # Find all the label widgets
+        self.lbl_name = self.findChild(QLabel, "lbl_detail_name")
+        self.lbl_director = self.findChild(QLabel, "lbl_detail_director")
+        self.lbl_release_date = self.findChild(QLabel, "lbl_detail_release_date")
+        self.lbl_genre = self.findChild(QLabel, "lbl_detail_genre")
+        self.lbl_description = self.findChild(QLabel, "lbl_detail_description")
+        self.lbl_rating = self.findChild(QLabel, "lbl_detail_rating")
+        self.lbl_duration = self.findChild(QLabel, "lbl_detail_duration")
+        self.lbl_age_rating = self.findChild(QLabel, "lbl_detail_age_rating")
+        self.lbl_main_actor = self.findChild(QLabel, "lbl_detail_main_actor")
+        self.lbl_image = self.findChild(QLabel, "lbl_detail_image")
+        
+        # Set the text for each label
+        self.lbl_name.setText(movie["title"])
+        self.lbl_director.setText(f"Director: {movie.get('director', 'N/A')}")
+        self.lbl_release_date.setText(f"Release Date: {movie.get('release_date', 'N/A')}")
+        self.lbl_genre.setText(f"Genre: {movie.get('genre', 'N/A')}")
+        self.lbl_rating.setText(f"Rating: {movie.get('rating', 'N/A')}")
+        self.lbl_duration.setText(f"Duration: {movie.get('duration', 'N/A')}")
+        self.lbl_age_rating.setText(f"Age Rating: {movie.get('age_rating', 'N/A')}")
+        self.lbl_main_actor.setText(f"Main Actor: {movie.get('main_actor', 'N/A')}")
+        
+        # Set the banner image
+        if movie.get('banner'):
+            self.lbl_image.setPixmap(QPixmap(movie["banner"]))
+        
+        # Format and set the description with word wrapping
+        description = movie.get("description", "No description available")
+        split_description = description.split(" ")
+        description = "\n".join([" ".join(split_description[i:i+10]) for i in range(0, len(split_description), 10)])
+        self.lbl_description.setText(f"Description: {description}")
+        
+        # Find and setup favorite button
+        self.btn_favorite = self.findChild(QPushButton, "btn_favorite")
+        if self.btn_favorite:
+            is_fav = is_favorite(self.user_id, movie_id)
+            icon = QIcon("img/heart-solid.svg" if is_fav else "img/heart-regular.svg")
+            self.btn_favorite.setIcon(icon)
+            self.btn_favorite.clicked.connect(lambda: self.toggle_favorite(movie_id))
+        
+        # Switch to detail page
+        self.stackedWidget.setCurrentIndex(1)
+        
+    def toggle_favorite(self, movie_id):
+        is_fav = is_favorite(self.user_id, movie_id)
+        if is_fav:
+            remove_from_favorites(self.user_id, movie_id)
+            self.btn_favorite.setIcon(QIcon("img/heart-regular.svg"))
+        else:
+            add_to_favorites(self.user_id, movie_id)
+            self.btn_favorite.setIcon(QIcon("img/heart-solid.svg"))
+        
+    def loadVideo(self):
+        if self.movie_id is None:
+            return
+        try:
+            movie = get_video_by_id(self.movie_id)
+            # Normalize video path
+            video_path = normalize_path(movie["video_path"])
+            self.mediaPlayer.setSource(QUrl.fromLocalFile(video_path))
+            self.mediaPlayer.play()
+            self.lbl_title.setText(movie["title"])
+            self.durationBar.sliderMoved.connect(self.setPosition)
+            self.volumeBar.sliderMoved.connect(self.setVolume)
+            self.mediaPlayer.playbackStateChanged.connect(self.mediaStateChanged)
+            self.mediaPlayer.positionChanged.connect(self.positionChanged)
+            self.mediaPlayer.durationChanged.connect(self.durationChanged)
+            self.mediaPlayer.errorOccurred.connect(self.handleError)
+            self.stackedWidget.setCurrentIndex(3)
+        except Exception as e:
+            print(f"Error loading video: {e}")
+            self.msg.error_message(f"Could not load video: {str(e)}")
+        
+    def mediaStateChanged(self):
+        if self.mediaPlayer.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
+            self.playBtn.setIcon(self.pauseIcon)
+        else:
+            self.playBtn.setIcon(self.playIcon)
+
+    def positionChanged(self, position):
+        self.durationBar.setValue(position)
+        current_time = self.formatTime(position)
+        total_time = self.formatTime(self.mediaPlayer.duration())
+        self.timeLabel.setText(f"{current_time}/{total_time}")
+        
+    def durationChanged(self):
+        self.durationBar.setRange(0, self.mediaPlayer.duration())
+    
+    def handleError(self):
+        self.playBtn.setEnabled(False)
+        error_message = self.mediaPlayer.errorString()
+        self.playBtn.setText(f"Error: {error_message}")
+        print(f"Media Player Error: {error_message}")
+        
+    def play(self):
+        if self.mediaPlayer.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+    
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+        
+    def setVolume(self, volume):
+        volume = volume / 100.0
+        self.audioOutput.setVolume(volume)
+        if volume == 0.0:
+            self.volumeBtn.setIcon(self.volumeOffIcon)
+        elif volume < 0.5:
+            self.audioOutput.setMuted(False)
+            self.volumeBtn.setIcon(self.volumeLowIcon)
+        else:
+            self.volumeBtn.setIcon(self.volumeHighIcon)
+            self.audioOutput.setMuted(False)
+    
+    def toggleMute(self):
+        if self.audioOutput.isMuted():
+            self.audioOutput.setMuted(False)
+            if self.current_volume >= 50:
+                self.volumeBtn.setIcon(self.volumeHighIcon)
+            elif self.current_volume < 50:
+                self.volumeBtn.setIcon(self.volumeLowIcon)
+            else:
+                self.volumeBtn.setIcon(self.volumeOffIcon)
+            self.volumeBar.setValue(self.current_volume)
+        else:
+            self.audioOutput.setMuted(True)
+            self.volumeBtn.setIcon(self.volumeOffIcon)
+            self.current_volume = self.volumeBar.value()
+            self.volumeBar.setValue(0)
+    
+    def toggleFullscreen(self):
+        if self.isFullScreen():
+            self.showNormal()
+        else:
+            self.showFullScreen()
+
+    def formatTime(self, milliseconds):
+        total_seconds = milliseconds // 1000
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+    
+    def catch_play_movie(self, movie_id):
+        self.movie_id = movie_id
+        self.loadVideo()
+
+    def catch_detail_movie(self, movie_id):
+        self.movie_id = movie_id
+        self.detail_movie(self.movie_id)
+        self.stackedWidget.setCurrentIndex(1)
+                
+    def navigateScreen(self, page:int):
+        self.stackedWidget.setCurrentIndex(page)
+
+    def showFavorites(self):
+        # Get favorite movies
+        favorite_movies = get_user_favorites(self.user_id)
+        
+        # Clear previous items
+        for i in reversed(range(self.favoriteLayout.count())):
+            widgetToRemove = self.favoriteLayout.itemAt(i).widget()
+            if widgetToRemove:
+                self.favoriteLayout.removeWidget(widgetToRemove)
+                widgetToRemove.setParent(None)
+                
+        if not favorite_movies:
+            # Show empty message
+            label = QLabel("You haven't added any titles to your list yet.")
+            label.setStyleSheet("color: white; font-family: Geist; font-size: 16px;")
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            self.favoriteLayout.addWidget(label)
+        else:
+            # Add movies to grid
+            row = 0
+            column = 0
+            for movie in favorite_movies:
+                itemWidget = MovieItemWidget(
+                    movie["id"], 
+                    movie["title"], 
+                    movie["banner"],  
+                    movie["video_path"],  
+                    movie.get("description", "")
+                )
+                itemWidget.set_user_id(self.user_id)  # Set user_id for the widget
+                itemWidget.signal_detail_movie.connect(self.catch_detail_movie)
+                itemWidget.signal_play_movie.connect(self.catch_play_movie)
+                itemWidget.signal_favorite_changed.connect(self.on_favorite_changed)  # Connect new signal
+                self.favoriteLayout.addWidget(itemWidget, row, column)
+                column += 1
+                if column == 3:
+                    row += 1
+                    column = 0
+        
+        # Switch to favorites page
+        self.stackedWidget.setCurrentIndex(2)
+        
+    def on_favorite_changed(self):
+        # If we're on the favorites page, refresh it
+        if self.stackedWidget.currentIndex() == 2:
+            self.showFavorites()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- setup_db.py
+++ setup_db.py
@@ -58,3 +58,74 @@ def update_user_in_db(user_id, name, gender, email, telephone, birthday):
     cursor.execute("UPDATE users SET name = ?, gender = ?, email = ?, telephone = ?, birthday = ? WHERE id = ?", (name, gender, email, telephone, birthday, user_id))
     conn.commit()
     conn.close()
+
+def get_all_videos():
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.row_factory = dict_factory
+    cursor.execute('SELECT * FROM video')
+    videos = cursor.fetchall()
+    conn.close()
+    return videos
+
+def get_video_by_id(video_id):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.row_factory = dict_factory
+    cursor.execute('SELECT * FROM video WHERE id = ?', (video_id,))
+    video = cursor.fetchone()
+    conn.close()
+    return video
+
+def get_video_by_name(name):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.row_factory = dict_factory
+    cursor.execute('SELECT * FROM video WHERE title LIKE ?', (f'%{name}%',))
+    videos = cursor.fetchall()
+    conn.close()
+    return videos
+
+def add_to_favorites(user_id, video_id):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('INSERT INTO favorite(user_id, video_id) VALUES (?, ?)', (user_id, video_id))
+        conn.commit()
+        return True
+    except sqlite3.IntegrityError:
+        return False
+    finally:
+        conn.close()
+
+def remove_from_favorites(user_id, video_id):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM favorite WHERE user_id = ? AND video_id = ?', (user_id, video_id))
+    conn.commit()
+    conn.close()
+
+def get_user_favorites(user_id):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.row_factory = dict_factory
+    
+    # First get all favorite video_ids for this user
+    cursor.execute('SELECT video_id FROM favorite WHERE user_id = ?', (user_id,))
+    favorite_video_ids = [row['video_id'] for row in cursor.fetchall()]
+    
+    # Then get all videos and filter
+    cursor.execute('SELECT * FROM video')
+    all_videos = cursor.fetchall()
+    conn.close()
+    
+    # Filter videos that are in favorite_video_ids
+    return [video for video in all_videos if video['id'] in favorite_video_ids]
+
+def is_favorite(user_id, video_id):
+    conn = sqlite3.connect('./data/database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT 1 FROM favorite WHERE user_id = ? AND video_id = ?', (user_id, video_id))
+    result = cursor.fetchone() is not None
+    conn.close()
+    return result
