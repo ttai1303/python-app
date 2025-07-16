4ddbd0783c21e5aae53049133586e21a557e642e
xbinh <binhnguyenxuan47@gmail.com>
2025-07-16 22:39:02 +0700

    fix

--- UI/mainwindow_ui.py
+++ UI/mainwindow_ui.py
@@ -312,7 +312,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        self.main_widget.setCurrentIndex(2)
+        self.main_widget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
